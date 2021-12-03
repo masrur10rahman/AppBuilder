e4fa8c0acf28c2390b926a6b5e713e8105be3901
masrur10rahman <masrur10rahman@gmail.com>
2021-12-03 17:12:32 +0600

    Deleted unneccessary 'java' files

--- app/src/main/java/com/show/text/DisplayMessageActivity.java
+++ app/src/main/java/com/show/text/DisplayMessageActivity.java
@@ -4,24 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.*;
 import android.os.*;
 import android.view.*;
-import android.view.View.*;
 import android.widget.*;
 import android.content.*;
-import android.graphics.*;
-import android.media.*;
-import android.net.*;
-import android.text.*;
-import android.util.*;
-import android.webkit.*;
-import android.animation.*;
-import android.view.animation.*;
-import java.util.*;
-import java.text.*;
-import android.widget.LinearLayout;
-import android.widget.Button;
-import android.widget.TextView;
-import android.content.Intent;
-import android.net.Uri;
 import android.view.View;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -34,92 +18,9 @@ public class DisplayMessageActivity extends AppCompatActivity {
 	private Button button1;
 	private TextView textview1;
 	
-	private Intent display_message = new Intent();
 	@Override
 	protected void onCreate(Bundle _savedInstanceState) {
 		super.onCreate(_savedInstanceState);
 		setContentView(R.layout.display_message);
-		initialize(_savedInstanceState);
-		initializeLogic();
 	}
-	
-	private void initialize(Bundle _savedInstanceState) {
-		
-		linear1 = (LinearLayout) findViewById(R.id.linear1);
-		button1 = (Button) findViewById(R.id.button1);
-		textview1 = (TextView) findViewById(R.id.textview1);
-		
-		button1.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View _view) {
-				display_message.setClass(getApplicationContext(), MainActivity.class);
-			}
-		});
-	}
-	private void initializeLogic() {
-		textview1.setText(text);
-	}
-	
-	@Override
-	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
-		super.onActivityResult(_requestCode, _resultCode, _data);
-		
-		switch (_requestCode) {
-			
-			default:
-			break;
-		}
-	}
-	
-	@Deprecated
-	public void showMessage(String _s) {
-		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
-	}
-	
-	@Deprecated
-	public int getLocationX(View _v) {
-		int _location[] = new int[2];
-		_v.getLocationInWindow(_location);
-		return _location[0];
-	}
-	
-	@Deprecated
-	public int getLocationY(View _v) {
-		int _location[] = new int[2];
-		_v.getLocationInWindow(_location);
-		return _location[1];
-	}
-	
-	@Deprecated
-	public int getRandom(int _min, int _max) {
-		Random random = new Random();
-		return random.nextInt(_max - _min + 1) + _min;
-	}
-	
-	@Deprecated
-	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
-		ArrayList<Double> _result = new ArrayList<Double>();
-		SparseBooleanArray _arr = _list.getCheckedItemPositions();
-		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
-			if (_arr.valueAt(_iIdx))
-			_result.add((double)_arr.keyAt(_iIdx));
-		}
-		return _result;
-	}
-	
-	@Deprecated
-	public float getDip(int _input){
-		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
-	}
-	
-	@Deprecated
-	public int getDisplayWidthPixels(){
-		return getResources().getDisplayMetrics().widthPixels;
-	}
-	
-	@Deprecated
-	public int getDisplayHeightPixels(){
-		return getResources().getDisplayMetrics().heightPixels;
-	}
-	
 }
